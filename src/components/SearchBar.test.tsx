189bac2790be9452027ef5c6fa755a1a27c4be9b
Manny <manny.blum@gmail.com>
2025-08-16 19:36:44 -0400

    fixing searchbar text

--- src/components/SearchBar.test.tsx
+++ src/components/SearchBar.test.tsx
@@ -1,26 +1,23 @@
-import { describe, expect, it, vi, beforeEach, afterEach } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SearchBar from "./SearchBar";
 import ProviderWrapper from "@/context/ProviderWrapper";
+import { MoviesContext } from "@/context/MoviesContext";
 
 describe("SearchBar", () => {
-  beforeEach(() => {
-    render(<SearchBar />, { wrapper: ProviderWrapper });
-  });
-
-  afterEach(() => {
-    cleanup();
-  });
-
   it("should render the SearchBar component", () => {
+    render(<SearchBar />, { wrapper: ProviderWrapper });
+
     const inputNode = screen.queryByLabelText("searchbar-label");
     expect(inputNode).toBeDefined();
   });
 
   it("should trigger handleChange as the user types", async () => {
+    render(<SearchBar />, { wrapper: ProviderWrapper });
+
     const user = userEvent.setup();
 
     const input = screen.getByPlaceholderText("Search for movies...");
@@ -30,6 +27,8 @@ describe("SearchBar", () => {
   });
 
   it("should focus input field if its empty", async () => {
+    render(<SearchBar />, { wrapper: ProviderWrapper });
+
     const user = userEvent.setup();
 
     const button = screen.getByRole("button");
@@ -42,17 +41,31 @@ describe("SearchBar", () => {
   });
 
   it("should handle the form submission", async () => {
+    const mockDispatch = vi.fn();
+
+    render(
+      <MoviesContext.Provider
+        value={{
+          state: { searchTerm: "", movies: null, genres: null },
+          dispatch: mockDispatch,
+        }}
+      >
+        <SearchBar />
+      </MoviesContext.Provider>
+    );
+
     const user = userEvent.setup();
 
-    const button = screen.getByRole("button");
+    const button = screen.getByRole("button", { name: /Search/i });
     const input = screen.getByPlaceholderText("Search for movies...");
-
-    const logSpy = vi.spyOn(console, "log").mockImplementation(() => {});
 
     await user.type(input, "batman");
     await user.click(button);
 
-    expect(logSpy).toHaveBeenCalledWith("searchTerm: ", "batman");
-    expect(button).toBeDefined();
+    expect(mockDispatch).toHaveBeenCalledOnce();
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: "SET_SEARCH_TERM",
+      payload: "batman",
+    });
   });
 });
