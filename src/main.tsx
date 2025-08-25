0331c024a5cf40b0a3428ffd9ddd16ee6d101482
Manny <manny.blum@gmail.com>
2025-08-25 13:25:24 -0400

    trying out memory router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import MoviesApp from "./MoviesApp.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, MemoryRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/movies">
+    <MemoryRouter>
       <MoviesApp />
-    </BrowserRouter>
+    </MemoryRouter>
   </StrictMode>
 );
